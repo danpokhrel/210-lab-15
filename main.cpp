23c4b0c698ac3de3e823843038858753047efe8c
Dan <texhnistguru@gmail.com>
2024-09-28 22:31:28 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 // COMSC-210 | Lab 15 | Dan Pokhrel
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
 using namespace std;
 
+class Movie {
+private:
+    string title;
+    int year;
+    string writer;
+public:
+    // getters and setters
+    string getTitle() const {return title;}
+    void setTitle(string t) {title = t;}
+    int getYear() const {return year;}
+    void setYear(int y) {year = y;}
+    string getName() const {return writer;}
+    void setWriter(string w) {writer = w;}
+
+    // Formatted Output
+    void print(){
+        cout << "Movie: " << title << endl;
+        cout << "\tYear Released: " << year << endl;
+        cout << "\tScreenwritier: " << writer << endl << endl;
+    }
+};
+
 int main(){
-    cout << "Hello World";
+    // Vector to hold movies
+    vector<Movie> movies;
+
+    // Read Data File
+    ifstream fin("inputs.txt");
+    if (!fin.good()) cout << "Input file not found.";
+    while (fin.good()){
+        
+    }
 
     return 0;
 }
